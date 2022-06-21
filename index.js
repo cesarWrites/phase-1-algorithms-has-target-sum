256d8c5d4961313551155ddb9ee6bf7cb238c8c4
Sarah Mukuti <cesarwrites24@gmail.com>
2022-06-21 16:35:04 +0300

    commit tree c240d683f24ac16d042cf16fb73feea7490fea79

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
 
+// Write your algorithm here
+function hasTargetSum(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) 
+      return true;
+    }
+  }
+  return false;
+}
 /* 
   Write the Big O time complexity of your function here
-*/
-
-/* 
+  The nested loop creates a time complexity of 
+  =O(n * n)
+ = O(n^2)
+ 
+}
+ 
   Add your pseudocode here
+  create teh fnction
+  pass two arguments an array and a target
+  ompare whether teh sum of any pair of elements equas to the target.
+  If equal to target return true
+  else return false
 */
 
 /*
   Add written explanation of your solution here
+  get the first number and subtract its complement from the target. 
+  If  the second number is equal to the complimnet then return true. 
+  If the compliment is not equal to the second number go to the next number and contiinue the process
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +48,24 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+  console.log("");
+
+
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([-7, 10, 4, 8],3));
+console.log("");
+
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([1, 2, 3, 4],5));
+console.log("");
+
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([2, 2, 3, 3], 4));
+console.log("");
+
+console.log("Expecting: false");
+console.log("=>", hasTargetSum([4],4));
+console.log("");
 }
 
 module.exports = hasTargetSum;
